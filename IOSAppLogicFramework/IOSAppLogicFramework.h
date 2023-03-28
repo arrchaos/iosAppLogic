69bfcea6d81aa766f3f2663ecd215d1fe696387e
Илья Федоров <ilya.fedorov@dipocket.org>
2023-03-28 19:10:15 +0300

    [IMPL](IOSAppLogicFramework): initial comit

---
+++ IOSAppLogicFramework/IOSAppLogicFramework.h
@@ -0,0 +1,18 @@
+//
+//  IOSAppLogicFramework.h
+//  IOSAppLogicFramework
+//
+//  Created by Илья on 28.03.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IOSAppLogicFramework.
+FOUNDATION_EXPORT double IOSAppLogicFrameworkVersionNumber;
+
+//! Project version string for IOSAppLogicFramework.
+FOUNDATION_EXPORT const unsigned char IOSAppLogicFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IOSAppLogicFramework/PublicHeader.h>
+
+
